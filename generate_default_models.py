716ad7e11b2fbe57f99c9cb35eeffdc84c6a26a6
Andrew Forde <awforde@gmail.com>
2023-11-14 14:33:04 -0800

    Updated arguments to use mnist_mlp; added models to generate_default_models

--- generate_default_models.py
+++ generate_default_models.py
@@ -1,6 +1,10 @@
 from federated_learning.arguments import Arguments
 from federated_learning.nets import Cifar10CNN
 from federated_learning.nets import FashionMNISTCNN
+from federated_learning.nets import Cifar10MLP
+from federated_learning.nets import MnistMLP
+from federated_learning.nets import CustomResNet
+from federated_learning.nets import CustomVisionTransformer
 import os
 import torch
 from loguru import logger
@@ -21,3 +25,27 @@ if __name__ == '__main__':
     # ---------------------------------
     full_save_path = os.path.join(args.get_default_model_folder_path(), "FashionMNISTCNN.model")
     torch.save(FashionMNISTCNN().state_dict(), full_save_path)
+
+    # ---------------------------------
+    # ----------- Cifar10MLP ----------
+    # ---------------------------------
+    full_save_path = os.path.join(args.get_default_model_folder_path(), "Cifar10MLP.model")
+    torch.save(Cifar10MLP().state_dict(), full_save_path)
+
+    # ---------------------------------
+    # ------------ MnistMLP -----------
+    # ---------------------------------
+    full_save_path = os.path.join(args.get_default_model_folder_path(), "MnistMLP.model")
+    torch.save(MnistMLP().state_dict(), full_save_path)
+
+    # ---------------------------------
+    # ---------- CustomResNet ---------
+    # ---------------------------------
+    full_save_path = os.path.join(args.get_default_model_folder_path(), "CustomResNet.model")
+    torch.save(CustomResNet().state_dict(), full_save_path)
+
+    # ---------------------------------
+    # ---- CustomVisionTransformer ----
+    # ---------------------------------
+    full_save_path = os.path.join(args.get_default_model_folder_path(), "CustomVisionTransformer.model")
+    torch.save(CustomVisionTransformer().state_dict(), full_save_path)
